a8dcc5f5949e7d264fcc039e8eebc1f58de3ea2f
noxor0 <avaterq0@gmail.com>
2018-02-13 22:51:12 -0800

    Stars falling perfectly

--- canvas-gravity/dist/js/canvas.bundle.js
+++ canvas-gravity/dist/js/canvas.bundle.js
@@ -86,11 +86,14 @@ var mouse = {
   y: innerHeight / 2
 };
 
-var gravity = 1;
+var gravity = .85;
 var friction = .6;
+var floor = 0;
+var dxValue = 7;
+var ballArray = [];
 
 addEventListener('click', function () {
-  init();
+  skyFall();
 });
 
 addEventListener('resize', function () {
@@ -116,19 +119,26 @@ function distance(x1, y1, x2, y2) {
 }
 
 // Objects;
-function Ball(x, y, dx, dy, radius, color) {
+function Ball(x, y, dx, dy, radius, ballArray) {
   this.x = x;
   this.y = y;
   this.dx = dx;
   this.dy = dy;
   this.radius = radius;
+  this.ballArray = ballArray;
   this.color = "rgba(224, 225, 255, 1)";
-  this.glow = "rgba(224, 225, 255, .01)";
+  this.glow = "rgba(224, 225, 255, .09)";
+
+  // this.glow = "rgba(40, 41, 56, 0)";
 
   this.update = function () {
-    if (this.y + this.radius + this.dy > canvas.height) {
+    if (this.y + this.radius + this.dy > canvas.height - floor) {
       this.dy = -this.dy * friction;
-      this.radius /= 2;
+      this.radius *= .6;
+      if (ballArray) {
+        // Parent
+        this.spawnChildren();
+      }
     } else {
       this.dy += gravity;
     }
@@ -136,49 +146,72 @@ function Ball(x, y, dx, dy, radius, color) {
     if (this.x + this.radius + this.dx > canvas.width || this.x - this.radius + this.dx < 0) {
       this.dx = -this.dx;
     }
+
     this.x += this.dx;
     this.y += this.dy;
     this.draw();
   };
 
   this.draw = function () {
-    if (this.radius > 1) {
-      var gradStar = c.createRadialGradient(this.x, this.y, this.radius / 2, this.x, this.y, this.radius * 2);
+    var gradStar = c.createRadialGradient(this.x, this.y, this.radius * .1, this.x, this.y, this.radius * 2);
 
-      gradStar.addColorStop(0, this.color);
-      gradStar.addColorStop(1, this.glow);
-      c.beginPath();
-      c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-      c.fillStyle = gradStar;
-      c.fill();
-      c.closePath();
+    gradStar.addColorStop(0, this.glow);
+    gradStar.addColorStop(1, this.glow);
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius * 1.3, 0, Math.PI * 2, false);
+    c.fillStyle = this.glow;
+    c.fill();
+    c.closePath();
+
+    gradStar.addColorStop(0, this.color);
+    gradStar.addColorStop(.5, this.glow);
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    c.fillStyle = gradStar;
+    c.fill();
+    c.closePath();
+  };
+
+  this.spawnChildren = function () {
+    for (var i = 0; i < randomIntFromRange(4, 8); i++) {
+      var childRadius = 3;
+      var dxChild = randomIntFromRange(-dxValue, dxValue);
+      var dyChild = randomIntFromRange(this.dy * .3, this.dy * .9);
+      ballArray.push(new Ball(this.x, this.y, dxChild, dyChild, childRadius, 0));
     }
   };
 }
 
-// Implementation;
-var ballArray = void 0;
+function skyFall() {
+  var radius = randomIntFromRange(8, 16);
+  var x = randomIntFromRange(radius, canvas.width - radius);
+  var y = radius + 10;
+  var dx = randomIntFromRange(-dxValue, dxValue);
+  var dy = randomIntFromRange(10, 20);
+  ballArray.push(new Ball(x, y, dx, dy, radius, ballArray));
+}
+
 function init() {
-  ballArray = [];
-  for (var i = 0; i < 100; i++) {
-    var radius = 10;
-    var x = randomIntFromRange(radius, canvas.width - radius);
-    var y = radius + 10;
-    var dx = randomIntFromRange(-3, 3);
-    var dy = randomIntFromRange(-2, 10);
-    ballArray.push(new Ball(x, y, dx, dy, radius));
+  for (var i = 0; i < randomIntFromRange(1, 5); i++) {
+    skyFall();
   }
 }
 
-// Animation Loop;
 function animate() {
   requestAnimationFrame(animate);
   c.clearRect(0, 0, canvas.width, canvas.height);
-  ballArray.forEach(function (ball) {
-    ball.update();
-  });
+
+  for (var i = 0; i < ballArray.length; i++) {
+    if (ballArray[i].radius < 1) {
+      ballArray.splice(i, 1);
+    }
+    if (ballArray.length > 0) {
+      ballArray[i].update();
+    }
+  }
 }
 
+setInterval(skyFall, 2000);
 init();
 animate();
 
